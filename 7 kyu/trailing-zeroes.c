0b243b9d09d2644c67af79373057fd4d122057d2
capyfara <115417692+lilydubu@users.noreply.github.com>
2025-03-01 14:46:53 +0800

    Create trailing-zeroes.c

---
+++ 7 kyu/trailing-zeroes.c
@@ -0,0 +1,15 @@
+// Given a number n, find the number of trailing zeros in its binary representation.
+
+// If n = 0 -> 1 zero
+// When we n & 1 -> checks if last bit is 1
+// If last bit is not one, keep counting
+
+unsigned trailing_zeroes(unsigned n) {
+    int zeroes = 0;
+    do{
+      if((n & 1) == 1) break;
+      zeroes++;
+      n = n >> 1; 
+    }while(n != 0);
+    return zeroes;
+}
